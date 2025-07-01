aae1b34a758e6e63ff4bd2a382759861e802cd78
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-01 23:54:35 +0500

    Mounted the express.static() middleware to the path /public with app.use(). The absolute path to the assets folder is __dirname + /public

--- basic-node-and-express/boilerplate-express-main/myApp.js
+++ basic-node-and-express/boilerplate-express-main/myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 
+app.use('/public', express.static(__dirname + '/public'));
 const htmlFilePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
     res.sendFile(htmlFilePath);
