599f8a4cae44b125cac693364a80d016a19dcaea
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-01 23:05:55 +0500

    Sent the /views/index.html file as a response to GET requests to the / path. it shows big HTML heading (and a form that we will use later…), with no style applied.

--- basic-node-and-express/boilerplate-express-main/myApp.js
+++ basic-node-and-express/boilerplate-express-main/myApp.js
@@ -2,8 +2,9 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 
+const htmlFilePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    res.sendFile(htmlFilePath);
 })
 
 
