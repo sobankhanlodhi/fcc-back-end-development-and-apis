73b9b39b7537ccf43f8354f9620ecbcf6e092169
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-05 19:24:17 +0500

    Modified the findAndUpdate function to find a person by Name and set the person's age to 20. Used the function parameter personName as the search key.
    Return the updated document. for that, we passed the options document { new: true } as the 3rd argument to findOneAndUpdate(). By default, these methods return the unmodified object.

--- boilerplate-mongomongoose-main/myApp.js
+++ boilerplate-mongomongoose-main/myApp.js
@@ -72,8 +72,14 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, 
+    { age: ageToSet },
+    { new: true },
+    (err, data) => {
+      if (err) return done(err);
+      done(null, data);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
