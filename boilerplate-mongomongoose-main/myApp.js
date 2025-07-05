5fbd533cc036f3a93f6e77d01208ba50ae612891
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-05 19:30:04 +0500

    findByIdAndRemove and findOneAndRemove are like the previous update methods. They pass the removed document to the db. We use the function argument personId as the search key.
    Modified the removeById function to delete one person by the person's _id. You should use one of the methods findByIdAndRemove() or findOneAndRemove().

--- boilerplate-mongomongoose-main/myApp.js
+++ boilerplate-mongomongoose-main/myApp.js
@@ -83,7 +83,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({_id: personId}, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  })
 };
 
 const removeManyPeople = (done) => {
