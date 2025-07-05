7b256379da3f4bfa971ad486ae1d284a5b2fc7fa
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-05 19:38:30 +0500

    Modified the queryChain function to find people who like the food specified by the variable named foodToSearch. Sort them by name, limit the results to two documents, and hide their age.
    Chained .find(), .sort(), .limit(), .select(), and then .exec(). Pass the done(err, data) callback to exec().
    
    If you don’t pass the callback as the last argument to Model.find() (or to the other search methods), the query is not executed. You can store the query in a variable for later use. This kind of object enables you to build up a query using chaining syntax. The actual db search is executed when you finally chain the method .exec(). You always need to pass your callback to this last method.

--- boilerplate-mongomongoose-main/myApp.js
+++ boilerplate-mongomongoose-main/myApp.js
@@ -99,8 +99,15 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) return done(err);
+      done(null, data);
+    });
+  
 };
 
 /** **Well Done !!**
