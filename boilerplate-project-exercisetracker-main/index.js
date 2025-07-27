16e009e290080567d78b01ef57d99da570cbaece
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-27 20:58:20 +0500

    Completed the Exercise Tracker API backend
    - Setup of Express.js server with Mongoose for MongoDB integration.
    - Defined Mongoose schemas for User, Exercise, and Log data models.
    - API endpoint for creating new users (`POST /api/users`).
    
    - API endpoint for retrieving all users (`GET /api/users`).
    
    - API endpoint for adding exercises to a user's log (`POST /api/users/:_id/exercises`).
    
    - API endpoint for retrieving a user's exercise log, including filtering by date range (`from`, `to`)
      and limiting results (`limit`) (`GET /api/users/:_id/logs`).
    
    - Basic error handling and response formatting for all endpoints.

--- boilerplate-project-exercisetracker-main/index.js
+++ boilerplate-project-exercisetracker-main/index.js
@@ -15,41 +15,158 @@ app.use(bodyParser.json());
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const urlSchema = new mongoose.Schema({
-  username: String
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true }
 })
-const USER_MODEL = mongoose.model('USER_MODEL', urlSchema);
+const USER_MODEL = mongoose.model('USER_MODEL', userSchema);
+
+const userExerciseSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'USER_MODEL'
+  }
+})
+const USER_EXERCISE_MODEL = mongoose.model('USER_EXERCISE_MODEL', userExerciseSchema);
+
+const userLOGSchema = new mongoose.Schema({
+  count: {
+    type: Number,
+    default: 0
+  },
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: Date
+    }
+  ],
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'USER_MODEL'
+  }
+})
+const USER_LOG_MODEL = mongoose.model('USER_LOG_MODEL', userLOGSchema);
 
 const usersGETHandler = async (req, res) => {
-  const userId = req.params._id
-  if (userId) {
-
-  } else {
-    const allUsers = USER_MODEL.find({})
-    let allUsersList = []
-    for await (const user of allUsers) {
-      allUsersList.push(user)
-    }
-    res.json(allUsersList)
+  try {
+    const allUsers = await USER_MODEL.find({})
+    res.json(allUsers)
+  } catch (error) {
+    console.log('\n usersGETHandler error is: ', error)
   }
 }
 
-const usersPOSTHandler = (req, res) => {
-  const newUser = new USER_MODEL({
-    username: req.body.username
-  })
-  newUser.save(function (err, data) {
-    if (err) return console.error(err)
-    res.json({
-      _id: data._id,
-      username: data.username
+const usersPOSTHandler = async (req, res) => {
+  try {
+    const newUser = new USER_MODEL({
+      username: req.body.username
     })
-  })
+    const savedUser = await newUser.save()
+
+    const newLog = new USER_LOG_MODEL({
+      userId: savedUser._id,
+      count: 0,
+      log: []
+    });
+    await newLog.save();
+
+    res.json({
+      _id: newUser._id,
+      username: newUser.username
+    })
+  } catch (error) {
+    console.log('\n usersPOSTHandler error is: ', error)
+  }
 }
 
-app.get('/api/users/:_id?', usersGETHandler)
-app.post('/api/users', usersPOSTHandler)
+const usersExercisePOSTHandler = async (req, res) => {
+  const userId = req.params._id
+  const { description, duration, date } = req.body
+  try {
+    const user = await USER_MODEL.findById(userId);
+    const exerciseDate = date ? new Date(date) : new Date();
+    const newExercise = new USER_EXERCISE_MODEL({
+      userId: userId,
+      description,
+      duration: duration,
+      date: exerciseDate
+    })
+    const savedExercise = await newExercise.save();
+    const userLog = await USER_LOG_MODEL.findOne({ userId: userId })
+    userLog.log.push({
+      description: savedExercise.description,
+      duration: savedExercise.duration,
+      date: savedExercise.date
+    })
+    userLog.count = userLog.log.length
+    await userLog.save()
 
+    res.json({
+      _id: user._id,
+      username: user.username,
+      date: savedExercise.date,
+      duration: savedExercise.duration,
+      description: savedExercise.description
+    })
+
+  } catch (error) {
+    console.log('\n usersExercisePOSTHandler error is: ', error)
+  }
+}
+
+const usersGETLogsHandler = async (req, res) => {
+  const userId = req.params._id
+  const { from, to, limit } = req.query
+  try {
+    const user = await USER_MODEL.findById(userId)
+    const userLog = await USER_LOG_MODEL.findOne({ userId: userId })
+    let filteredLog = userLog.log
+    if (from || to) {
+      const fromDate = from ? new Date(from) : new Date(0)
+      const toDate = to ? new Date(to) : newDate()
+      filteredLog = filteredLog.filter((exerciseDetail) => {
+        const exerciseDate = new Date(exerciseDetail.date)
+        return !isNaN(exerciseDate.getTime()) && exerciseDate >= fromDate && exerciseDate <= toDate
+      })
+    }
+    if (limit) {
+      filteredLog = filteredLog.slice(0, parseInt(limit))
+    }
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      count: filteredLog.length,
+      log: filteredLog.map((exercise) => {
+        return {
+          description: exercise.description,
+          duration: exercise.duration,
+          date: new Date(exercise.date).toDateString()
+        }
+      })
+    })
+
+  } catch (error) {
+    console.log('\n usersGETLogsHandler error is: ', error)
+  }
+}
+
+app.post('/api/users', usersPOSTHandler)
+app.get('/api/users/', usersGETHandler)
+app.post('/api/users/:_id/exercises', usersExercisePOSTHandler)
+app.get('/api/users/:_id/logs', usersGETLogsHandler)
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
