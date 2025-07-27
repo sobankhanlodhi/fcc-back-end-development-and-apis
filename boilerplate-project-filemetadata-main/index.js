4f279d88fc7d015cc068c125c0a7935d7b6ffdf6
sobankhanlodhi <sobankhanlodhi@gmail.com>
2025-07-27 21:35:14 +0500

    completed the filemetadata exercise with Multer
    HTML form have enctype="multipart/form-data" and name="upfile" properties
    This tells multer which field on the request it should look for the files in.
    
    multipart/form-data, which is primarily used for uploading files. Multer will not process any form which is not multipart (multipart/form-data).
    
    When we submit a file, we gave: the file name, type, and size in bytes within the JSON response.

---
+++ boilerplate-project-filemetadata-main/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+const fileUploadHandler = (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+}
+
+app.post('/api/fileanalyse', upload.single('upfile'), fileUploadHandler)
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
